b01b2da0a6c57c660572f839ce67621ece30417e
tristan biardeau <spi3085@mordor.univ-lemans.fr>
2017-03-08 11:40:40 +0100

    Add register

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^connexion$', views.connexion),
+    url(r'^register$', views.register),
 ]
--- user/views.py
+++ user/views.py
@@ -1,17 +1,45 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from user.forms import ConnexionForm
-from django.contrib.auth import authenticate, login
+from user.forms import RegisterForm
+from django.contrib.auth import authenticate, login, logout
 
 def connexion(request):
 
     form = ConnexionForm(request.POST or None)
-
     if form.is_valid():
         account = form.cleaned_data['account']
         password = form.cleaned_data['password']
         print(account, password)
+        # user = authenticate(account=account, password=password)
+        if user is not None:
+        	if user_can_authenticate(user):
+        		login(request, user)
+        	else:
+        		print ("L'utilisateur est banni")
+        else:
+        	print ("L'utilisateur n'existe pas")
     
 
 
     return render(request, 'user/connexion.html', locals())
+
+
+def register(request):
+
+    form = RegisterForm(request.POST or None)
+
+    if form.is_valid():
+        account = form.cleaned_data['account']
+        password = form.cleaned_data['password']
+        email = form.cleaned_data['email']
+        print(account, password, email)
+    
+
+
+    return render(request, 'user/register.html', locals())
+
+
+
+def logout_view(request):
+    logout(request)
