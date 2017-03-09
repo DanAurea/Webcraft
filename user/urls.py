d15f878a35b921b1ac19ca80a2ccb80d96457a31
= <laochi.bc@gmail.com>
2017-03-09 03:57:57 +0100

    User can now login and logout from application, update gitignore

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^login$', views.login),
+    url(r'^logout$', views.logout),
     url(r'^register$', views.register),
 ]
--- user/views.py
+++ user/views.py
@@ -2,25 +2,32 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from user.forms import RegisterForm
 from user.forms import LoginForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout 
+from django.contrib.auth.models import Permission
 from django.db import IntegrityError
+from django.core.urlresolvers import reverse
 
 def login(request):
 
-    form = LoginForm(request.POST or None)
+    form        = LoginForm(request.POST or None)
     valueButton = "Se connecter"
+    error       = None
+
     if form.is_valid():
         account = form.cleaned_data['account']
         password = form.cleaned_data['password']
-        print(account, password)
-        # user = authenticate(account=account, password=password)
+        ## Check if data fits with database
+        user = authenticate(username=account, password=password)
         if user is not None:
-        	if user_can_authenticate(user):
-        		login(request, user)
-        	else:
-        		print ("L'utilisateur est banni")
+            auth_login(request, user)
+            ## Check if user wasn't banned by an all mighty admin / moderator
+            ## Not working at moment, should be fixed later
+        	# if user_can_authenticate():
+        	# 	login(request, user)
+        	# else:
+        	# 	error="L'utilisateur est banni"
         else:
-        	print ("L'utilisateur n'existe pas")
+        	error="L'utilisateur n'existe pas"
         	registerLink = "<a href=\"register\" >Inscrivez-vous</a>"
 
     return render(request, 'user/baseForm.html', locals())
@@ -48,3 +55,4 @@ def register(request):
 
 def logout_view(request):
     logout(request)
+    return redirect(reverse(login))
