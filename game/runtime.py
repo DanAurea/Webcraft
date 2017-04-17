7cb74b469f4e01bdf71c550576d3e1a51d681b34
Valentin4311 <valentin.legeay@gmail.com>
2017-04-17 18:59:37 +0200

    Upcoming features

--- game/runtime.py
+++ game/runtime.py
@@ -1,63 +1,37 @@
-from game.mapGenerator import MapGenerator, Chunk
+from game.server.world.world import World
+from game.server.world.chunk import Chunk
+from game.server.gen.chunkGenerator import ChunkGenerator
 from datetime import datetime
 from os import path, remove
+from random import randint
+import random
 from glob import glob
 from django.core.cache import cache
 import json
 import pickle
-import game.mapGenerator as MapInfo
 
-""" Runtime module starting game loop and initialize game server side """
+def getSettings(key, defaultValue):
+	if(key in settings):
+		return settings[key]
+	else:
+		return defaultValue
 
-sizeChunk = 1024
-
-## Save interval in hours
-saveInterval = 2
-## When reset save and keep the last one in hours (by day currently)
-saveReset    = 24
-
-settings = {'generate': False}
-
-if(path.isfile('game/settings.json')):
-	with open('game/settings.json') as jsonData:
-		settings = json.load(jsonData)
-else:
-	print("Warning: No settings.json found in game app directory !")
-
-## Tick state
-if('time' in settings):
-	timeDay = settings['time']
-else:
-	timeDay = 0
-
-## Duration day based on tickstate (10 min here => 10 * 60 * 20 ticks/s)
-if('duration' in settings):
-	durationDay = settings['duration']
-else:
-	durationDay = 12000
-
-## Set size of map
-if('size' in settings):
-	size = settings['size']
-else:
-	size = 6
-	
 def generate():
-	"""Generate a new map"""
-	global map
-	print("Generating new map...")
-	map = MapGenerator(size).generate()
+	"""Generate a new world"""
+	global world
+	print("Generating new world...")
+	world = World(name, seed, ChunkGenerator(), size, size);
 	print("Done !")
 	pass
 
 def saveInFile():
-	"""Save map in a binary file """
-	global map
+	"""Save world in a binary file """
+	global world
 	now = datetime.now()
 	time = now.strftime("%Y-%m-%d-%H-%M-%S")
-	saveFile = "".join(['game/saves/DRPG-', time, '.map'])
+	saveFile = "".join(['game/saves/DRPG-', time, '.world'])
 
-	files = glob('game/saves/*.map')
+	files = glob('game/saves/*.world')
 	numberFiles = len(files)
 
 	## Remove all unecessary saves if enough saves had been made
@@ -67,60 +41,93 @@ def saveInFile():
 
 	# We reconstruct an empty from redis datas
 	# so we need to start from a new instance of
-	# map generator.
-	map = [[0 for row in range(size)] for col in range(size)]
+	# world generator.
+	world = [[0 for row in range(size)] for col in range(size)]
 
 	for i in range(0, size):
 		for j in range(0, size):
 			chunkInst = Chunk(i, j)
 			chunkInst.chunk = reassemble(i, j)
-			map[i][j] = chunkInst
+			world[i][j] = chunkInst
 
 	with open(saveFile, 'wb') as save:
-		pickle.dump(map, save)
+		pickle.dump(world, save)
 
 def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+	"""Yield successive n-sized chunks from l."""
+	for i in range(0, len(l), n):
+		yield l[i:i + n]
 
-def reassemble(x, z): 
-	"""	Reassemble a dispatched chunk from redis"""
+def reassemble(x, z):
+	"""	Reassemble clusters to a tile array from redis"""
 
-	nbDivisions = int(MapInfo.nbTilesByChunk / sizeChunk)
-	
-	chunk = []	
+	nbCluster = int(Chunk.TILES_PER_CHUNKS / clusterSize)
 
-	for i in range(0, nbDivisions):
-		key      = "".join(["map_", str(x), "_", str(z), "_", str(i)])
-		division = cache.get(key)
-		chunk.append(division)
+	tiles = []
 
-	chunk = [item for sublist in chunk for item in sublist]
+	for i in range(0, nbCluster):
+		key      = "".join(["cluster_", str(x), "_", str(z), "_", str(i)])
+		cluster = cache.get(key)
+		tiles.append(cluster)
 
-	return chunk
+	tiles = [item for sublist in tiles for item in sublist]
+
+	return tiles
 
 def subdivideMap():
-	"""Subdivide map chunks in data chunks for
+	"""Subdivide world chunks in data chunks for
 		a smoother update in redis cache."""
-	global map
-	for arr in map:
-			for chunkObj in arr:
-					chunkObj.chunk = list(chunks(chunkObj.chunk, sizeChunk))
+	global world
+	for chunk in world.chunks:
+		chunk.tileClusters = list(chunks(chunk.tiles, clusterSize))
+
 
 def saveInRedis():
-	"""Save in Redis all chunks previously subdivided."""
-	
-	for rowIndex, row in enumerate(map):
-		for colIndex, col in enumerate(row):
-			for chunkIndex, chunk in enumerate(col.chunk):
-				key = "".join(["map_", str(rowIndex), "_", str(colIndex), "_",str(chunkIndex)])
-				cache.set(key, chunk, timeout=None)
+	"""Save in Redis all chunks previously subdivided into clusters."""
+
+	nbCluster = int(Chunk.TILES_PER_CHUNKS / clusterSize)
+	for chunk in world.chunks:
+		for clusterIndex in range(0, nbCluster):
+			key = "".join(["cluster_", str(chunk.x), "_", str(chunk.z), "_",str(clusterIndex)])
+			cache.set(key, chunk.tileClusters[clusterIndex], timeout=None)
+
+def compressChunk(tiles):
+	""" Compress an array of tiles counting sequence of same numbers """
+	compressedChunk =  []
+	countVal = 1
+	buffer = tiles[0]
+	size = len(tiles) - 1
+
+	## Compressing loop with RLE encoding
+	index = 0
+	for tile in tiles:
+
+		if(index == size):
+
+			if(buffer != tile):
+				compressedChunk.append(str(tile))
+			else:
+				compressedChunk.append(str(countVal + 1) + ":" + str(buffer))
+
+		elif(buffer != tile):
+
+			if(countVal == 1):
+				compressedChunk.append(str(buffer))
+			else:
+				compressedChunk.append(str(countVal) + ":" + str(buffer))
+
+			buffer = tile
+			countVal = 1
+		else:
+			countVal += 1
+		index += 1
+
+	return compressedChunk
 
 def loadMap():
-	""" Load map from latest save """
-	global map
-	files      = glob('game/saves/*.map')
+	""" Load world from latest save """
+	global world
+	files      = glob('game/saves/*.world')
 
 	if(not files or settings['generate'] == True):
 		generate()
@@ -128,10 +135,43 @@ def loadMap():
 		latestSave = max(files, key=path.getctime)
 
 		with open(latestSave, 'rb') as save:
-			map = pickle.load(save)
+			world = pickle.load(save)
 
 	subdivideMap()
 	saveInRedis()
-		
+
 	if(not files or settings['generate'] == True):
 		saveInFile()
+
+
+""" Runtime module starting game loop and initialize game server side """
+
+clusterSize = 1024
+
+## Save interval in hours
+saveInterval = 2
+## When reset save and keep the last one in hours (by day currently)
+saveReset    = 24
+
+settings = {'generate': False, 'worldName': 'Cool name', 'worldSeed': 418416568}
+
+if(path.isfile('game/settings.json')):
+	with open('game/settings.json') as jsonData:
+		settings = json.load(jsonData)
+else:
+	print("Warning: No settings.json found in game app directory !")
+
+## Tick state
+timeDay = getSettings('time', 0)
+
+## Duration day based on tickstate (10 min here => 10 * 60 * 20 ticks/s)
+durationDay = getSettings('duration', 12000)
+
+## Set size of world
+size = getSettings('size', 6)
+
+## Set name of world
+name = getSettings('worldName', 'No name')
+
+## Set seed of world
+seed = getSettings('worldSeed', random.randint(0, 99999999999))
---
+++ game/server/gen/chunkGenerator.py
@@ -0,0 +1,63 @@
+from opensimplex import OpenSimplex
+from random import randint
+import math, sys, random
+
+from game.server.world.world import World
+from game.server.biomes.biome import *
+from game.server.tiles.tiles import *
+from game.server.world.chunk import Chunk
+
+class ChunkGenerator:
+
+	def setWorld(self, world):
+		self.world = world;
+		self.genNoiseElev = OpenSimplex(world.seed * 5824389085730)
+		self.genNoiseMoist = OpenSimplex(world.seed * 3674237955873)
+		self.genNoiseTemp = OpenSimplex(world.seed * 8209480480238)
+
+	def genChunk(self, chunk):
+		biome = None
+		for cX in range(Chunk.CHUNK_SIZE):
+			rX = cX + chunk.x * Chunk.CHUNK_SIZE
+
+			for cZ in range(Chunk.CHUNK_SIZE):
+				rZ= cZ + chunk.z * Chunk.CHUNK_SIZE
+
+				biome = self.getBiomeAt(rX, rZ)
+				height = self.getHeightAt(rX, rZ);
+				chunk.heightMap[cX][cZ] = height
+
+				for y in range(height):
+					chunk.setTileAt(self.getTileForDepth(cX, y, cZ, height, biome), cZ, y, cZ)
+
+		chunk.biome = biome
+		chunk.biome.generate(self.world, chunk)
+
+
+	def getTileForDepth(self, cX, cY, cZ, surface, biome):
+		depth = surface - cY
+
+		if(cY == 0):
+			return Tiles.BEDROCK
+		if(depth > 5):
+			return Tiles.STONE
+		elif(depth > 2):
+			return Tiles.DIRT
+		else:
+			return biome.floorTile
+
+	def getHeightAt(self, x, z):
+		return int((self.genNoiseElev.noise2d(x / 200, z / 200) / 2.0 + 0.5) * 30)
+
+	def getMoistAt(self, x, z):
+		return self.genNoiseMoist.noise2d(x, z) / 2.0 + 0.5
+
+	def getTemperatureAt(self, x, z):
+		return self.genNoiseTemp.noise2d(x, z) / 2.0 + 0.5
+
+	def getBiomeAt(self, x, z):
+		x /= 120
+		z /= 120
+		moist = self.getMoistAt(x, z)
+		temperature = self.getTemperatureAt(x, z)
+		return Biomes.getBiomeForClimate(moist, temperature)
